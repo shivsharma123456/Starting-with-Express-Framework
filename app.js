abac738bd210c4d3a46d0cfcaa1b9f5e1a88bdec
Shiv Sharma <shiv.yogesh92@gmail.com>
2023-05-07 14:49:35 +0530

    Simple Group Chat Application

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-const express = require('express');
-
-const bodyParser = require('body-parser');
+const express=require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+const loginRoute=require('./routes/login')
+const messageRoute = require('./routes/message')
 
-const adminRoutes = require('./routes/admin'); 
-const shopRoutes = require('./routes/shop');
+app.use(bodyParser.urlencoded({ extended:false}))
 
-app.use(bodyParser.urlencoded ({extended: false}));
-
-app.use('/admin', adminRoutes); 
-app.use(shopRoutes);
-
-app.use((req, res, next) => {
-
-    res.status(404).send('<h1>Page not found</h1>');
-});
-
-app.listen(3000);
+app.use(loginRoute)
+app.use(messageRoute)
+app.listen(3000)
