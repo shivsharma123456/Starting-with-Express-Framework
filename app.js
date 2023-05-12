a742e89613e92a40b235097b1757a7e3023c545e
Shiv Sharma <shiv.yogesh92@gmail.com>
2023-05-12 12:35:39 +0530

    What the hell are Controllers

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const ejs = require('ejs');
+
 const path=require('path');
 const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
 const app = express();
+
+app.set('view engine', 'ejs');
 
 const adminRoutes = require('./routes/admin'); 
 const shopRoutes = require('./routes/shop');
@@ -13,9 +18,6 @@ app.use(bodyParser.urlencoded ({extended: false}));
 app.use('/admin', adminRoutes); 
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
