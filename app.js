460a384ebb28e49c432b40285c912532f181ca71
Shiv Sharma <shiv.yogesh92@gmail.com>
2023-05-13 13:49:54 +0530

    Dynamic Routes Part 1

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-const ejs = require('ejs');
+const path = require('path');
 
-const path=require('path');
 const express = require('express');
-
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin'); 
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded ({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes); 
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 app.listen(3000);
+
+
+// express-handlebars
+    //pug
